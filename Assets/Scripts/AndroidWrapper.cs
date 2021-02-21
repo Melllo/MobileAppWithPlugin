a461c4b2d0cfc1c15d07a360644e940a7350d4e3
Irene Herbei <trisha19991410@gmail.com>
2021-02-21 23:41:59 +0200

    plugin

---
+++ Assets/Scripts/AndroidWrapper.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AndroidWrapper : MonoBehaviour
+{
+    const string CLASS_NAME = "com.example.androidplugin.PluginClass";
+    const string ACTIVITY = "com.unity3d.player.UnityPlayerNativeActivity";
+    static AndroidJavaClass plugin = new AndroidJavaClass(CLASS_NAME);
+
+    public  InputField firstNum; 
+    public  InputField secondNum;
+
+ 
+    public void Push()
+    {
+     AndroidJavaClass plugin = new AndroidJavaClass(CLASS_NAME);
+        if (plugin != null)
+        {
+#if UNITY_ANDROID
+            plugin.CallStatic("getToast", int.Parse(firstNum.text) + int.Parse(secondNum.text));
+#endif
+        }
+    
+    }
+}
